b1ea0732d1941e83040263d8c37afdf07a2912e8
andybirenzi <andybirenzi@gmail.com>
2017-06-01 18:44:56 -0400

    initilizating the project

---
+++ src/main/java/birenzi/commerce/springboot/BcommerceApplication.java
@@ -0,0 +1,12 @@
+package birenzi.commerce.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcommerceApplication.class, args);
+	}
+}
